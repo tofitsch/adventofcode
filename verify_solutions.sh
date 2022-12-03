632f1026460896a210b9333e8ba185b39720890f
tofitsch <tobias.fitschen@cern.ch>
2022-12-03 19:20:08 +0000

    make lint happy

--- verify_solutions.sh
+++ verify_solutions.sh
@@ -6,8 +6,8 @@ for day in day*; do
 
   for part in 1 2; do
 
-    computed_solution=`awk -f $day/solution_part$part.awk $day/input.txt`
-    true_solution=`sed -n "${part}p" < $day/solution.txt`
+    computed_solution=$(awk -f "$day/solution_part$part.awk" "$day/input.txt")
+    true_solution=$(sed -n "${part}p" < "$day/solution.txt")
 
     echo "$day part$part: $true_solution | $computed_solution" 
 
