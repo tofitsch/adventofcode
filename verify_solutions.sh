3048c2f1d6121fbdfa3bd60221179285d4e1c59d
tofitsch <tobias.fitschen@cern.ch>
2022-12-05 19:07:10 +0000

    account for the case where no true solution is provided yet

--- verify_solutions.sh
+++ verify_solutions.sh
@@ -6,8 +6,14 @@ for day in day*; do
 
   for part in 1 2; do
 
-    computed_solution=$(awk -f "$day/solution_part$part.awk" "$day/input.txt")
     true_solution=$(sed -n "${part}p" < "$day/solution.txt")
+
+    if ["$true_solution" == ""]; then
+      echo "[WARNING]: No true solution provided for $day part$part"
+      continue
+    fi
+
+    computed_solution=$(awk -f "$day/solution_part$part.awk" "$day/input.txt")
 
     echo "$day part$part: $true_solution | $computed_solution" 
 
