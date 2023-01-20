bafe93d5910b4b0190b9adb1741c0a638534b9c4
tofitsch <tobias.fitschen@cern.ch>
2023-01-20 08:30:09 +0000

    added vimscript solutions to verify script

--- verify_solutions.sh
+++ verify_solutions.sh
@@ -1,50 +1,69 @@
 #!/bin/bash
 
-for day in 2022/day*; do
-
-  for part in 1 2; do
-
-    true_solution=""
-
-    if test -f "$day/solution.txt"; then
-      true_solution=$(sed -n "${part}p" < "$day/solution.txt")
-    fi
-
-    if [ "$true_solution" == "" ]; then
-      echo "[WARNING]: No true solution provided for $day part$part"
-      continue
-    fi
-
-    {
-      IFS=$'\n' read -r -d '' time_for_solution;
-      IFS=$'\n' read -r -d '' computed_solution;
-    } < <((printf '\0%s\0' "$(time (awk -f "$day/part$part.awk" "$day/input.txt"))" 1>&2) 2>&1)
-
-    timing=$(echo $time_for_solution | awk '{print $4" + "$6}')
-
-    if [ "$day part$part" == "2022/day10 part2" ]; then
-
-      md5=$(echo $computed_solution| md5sum)
-
-      if [ "$md5" == "6fffc8e4a2a7b1b31cb7a9b6de5f7db0  -" ]; then
-        computed_solution="EHZFZHCZ"
-      else
-        computed_solution="wrong md5 sum"
+#for year in 2021 2022; do
+for year in 2021; do
+  for day in $year/day*; do
+  
+    for part in 1 2; do
+  
+      true_solution=""
+  
+      if test -f "$day/solution.txt"; then
+        true_solution=$(sed -n "${part}p" < "$day/solution.txt")
+      fi
+  
+      if [ "$true_solution" == "" ]; then
+        echo "[WARNING]: No true solution provided for $day part$part"
+        continue
       fi
 
-    fi
+      if [ "$year" == "2022" ]; then
+        {
+          ifs=$'\n' read -r -d '' time_for_solution;
+          ifs=$'\n' read -r -d '' computed_solution;
+        } < <((printf '\0%s\0' "$(time (awk -f "$day/part$part.awk" "$day/input.txt"))" 1>&2) 2>&1)
+        timing=$(echo $time_for_solution | awk '{print $4" + "$6}')
+      else
+        # there must be a better way of doing this... but getting both timing and output for a vimscript is not easy...
+        cd $day
 
-    echo "$day part$part : $timing : $computed_solution" 
+        test=$(((echo "X" ; time (vim --cmd ":so part$part.vim | quit")) 1>&2) 2>&1)
 
-    if [ "$computed_solution" != "$true_solution" ]; then
-      echo "[ERROR]: Mismatch of true and computed solution:"
-      echo "true: $true_solution"
-      echo "computed: $computed_solution"
-      return 1
-    fi
+        # horribly contrived way of stripping escape sequences:
+        echo $test > tmp
+        computed_solution=$(awk 'BEGIN{FS=""}; {for(i=NF-79; i>44; i--) printf $i}' tmp | rev)
+        rm tmp
 
+        t_u=$(echo $test | cut -d' ' -f15)
+        t_s=$(echo $test | cut -d' ' -f17)
+        timing=$(echo $t_u $t_s | awk '{print $1" + "$2}')
+
+        cd ../..
+      fi
+  
+  
+      if [ "$day part$part" == "2022/day10 part2" ]; then
+  
+        md5=$(echo $computed_solution| md5sum)
+  
+        if [ "$md5" == "6fffc8e4a2a7b1b31cb7a9b6de5f7db0  -" ]; then
+          computed_solution="EHZFZHCZ"
+        else
+          computed_solution="wrong md5 sum"
+        fi
+  
+      fi
+  
+      echo $year `basename $day` "part$part : $timing : $computed_solution" 
+  
+      if [ "$computed_solution" != "$true_solution" ]; then
+        echo "[ERROR]: Mismatch of true and computed solution:"
+        echo "true: $true_solution"
+        echo "computed: $computed_solution"
+        return 1
+      fi
+  
+    done
+  
   done
-
 done
-
-
