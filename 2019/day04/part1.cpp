d30914ffeb45f6277237c65d12ee25a2ffd7871a
tofitsch <tobias.fitschen@cern.ch>
2023-08-20 18:09:41 +0100

    2019 day04

---
+++ 2019/day04/part1.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+
+#define NUM_DIGITS 6
+
+using namespace std;
+
+int main(){
+
+  ifstream in_file("input.txt");
+
+  string line, field;
+
+  getline(in_file, line);
+
+  stringstream line_stream(line);
+
+  getline(line_stream, field, '-');
+
+  int min = stoi(field);
+
+  getline(line_stream, field, '-');
+
+  int max = stoi(field);
+
+  int ctr = 0;
+
+  for(int i=min; i<=max; i++){
+
+    char num_str[NUM_DIGITS + 1];
+    snprintf(num_str, NUM_DIGITS + 1, "%d", i);
+
+    bool valid = false;
+    
+    for(int i=1; i<NUM_DIGITS; i++){
+      
+      if(num_str[i] < num_str[i-1]){
+
+        valid = false;
+        break;
+
+      }
+
+      if(num_str[i] == num_str[i-1]) valid = true;
+
+    }
+
+    if(valid) ctr++;
+    
+  }
+
+  cout<<ctr<<endl;
+
+}
