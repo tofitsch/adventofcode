76940582289b7f33eae7502e0ba22c4a7673d818
tofitsch <tobias.fitschen@cern.ch>
2023-08-29 12:28:37 +0100

    2019 day14 part1

---
+++ 2019/day14/part1.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+map<string, int> quantities;
+map<string, vector<pair<string, int>>> recipes;
+
+void do_recipe(string product){
+  
+  quantities[product] += recipes[product].back().second;
+
+  for(int i=0; i<recipes[product].size() - 1; i++)
+    quantities[recipes[product][i].first] -= recipes[product][i].second;
+
+}
+
+int main(){
+
+  ifstream in_file("input.txt");
+
+  string line, field;
+
+  while(getline(in_file, line)){
+    
+    stringstream line_stream(line);
+
+    bool is_number = true;
+
+    vector<pair<string, int>> vec;
+
+    while(getline(line_stream, field, ' ')){
+      
+      if(field == "=>") continue;
+
+      if(field.back() == ',') field.pop_back();
+      
+      if(is_number) vec.push_back({"", stoi(field)});
+      else vec.back().first = field;
+
+      is_number = !is_number;
+      
+    }
+
+    recipes[vec.back().first] = vec;
+
+  }
+
+  quantities["FUEL"] = -1;
+
+  bool complete;
+
+  do{
+    
+    complete = true;
+
+    for(auto &x : quantities){
+
+      if(x.first == "ORE") continue;
+      
+      while(x.second < 0){
+
+        complete = false;
+
+        do_recipe(x.first);
+
+      }
+
+    }
+
+  }while(!complete);
+
+  cout<<-quantities["ORE"]<<endl;
+
+}
