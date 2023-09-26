ede2ba2dde32b0d639670b277f3314473ce4b1b8
tofitsch <tobias.fitschen@cern.ch>
2023-09-26 14:45:50 +0100

    graph pruning

--- 2019/day18/test.cpp
+++ 2019/day18/test.cpp
@@ -25,19 +25,44 @@ class Graph{
   private:
 
     T* nodes;
-    int n_nodes = 0;
     vector<Edge<T>> edges;
 
+    vector<int> get_connections(T*, bool);
+
   public:
+
+    int n_nodes = 0;
     
     Graph(int size) : nodes(new T[size]) {}
     ~Graph(){delete[] nodes;}
     
-
     void add_edge(T, T, int);
+    void add_edge_or_update_weight(T*, T*, int);
     void prune(vector<vector<char>> &);
+    void print(vector<vector<char>> &);
 
 };
+
+template <typename T>
+void Graph<T>::add_edge_or_update_weight(T* ptr_a, T* ptr_b, int weight){
+
+  for(Edge<T> & edge : edges){
+
+    if((edge.in == ptr_a && edge.out == ptr_b) ||
+       (edge.in == ptr_b && edge.out == ptr_a)
+    ){
+      
+      if(edge.weight > weight) edge.weight = weight;
+      
+      return;
+
+    }
+
+  }
+
+  edges.push_back(Edge<T>(ptr_a, ptr_b, weight));
+
+}
 
 template <typename T>
 void Graph<T>::add_edge(T a, T b, int weight){
@@ -68,7 +93,7 @@ void Graph<T>::add_edge(T a, T b, int weight){
 
   }
 
-  edges.push_back(Edge<T>(ptr_a, ptr_b, weight));
+  add_edge_or_update_weight(ptr_a, ptr_b, weight);
 
 }
 
@@ -94,6 +119,22 @@ vector<Coordinate> get_neighbours(Coordinate coord, vector<vector<char>> & grid)
 }
 
 template <typename T>
+vector<int> Graph<T>::get_connections(T* node, bool in){
+  
+ vector<int> connections;
+  
+  for(int e=0; e<edges.size(); e++){
+
+    if(!in && edges.at(e).in == node) connections.push_back(e);
+    if(in && edges.at(e).out == node) connections.push_back(e);
+     
+  }
+
+  return connections;
+
+}
+
+template <typename T>
 void Graph<T>::prune(vector<vector<char>> & grid){
   
   bool done = false;
@@ -102,23 +143,61 @@ void Graph<T>::prune(vector<vector<char>> & grid){
  
     done = true;
 
-    for(int e=0; e<edges.size(); e++){
-
-      T* node_in = edges.at(e).in;
-      T* node_out = edges.at(e).out;
+    for(int n=0; n<n_nodes; n++){
       
-      if(!isalpha(grid[node_in->first][node_in->second]) &&
-         !isalpha(grid[node_out->first][node_out->second])
-       ){
-        
+      char tile = grid[nodes[n].first][nodes[n].second];
+      
+      if(isalpha(tile) || tile == '@') continue;
+      
+      vector<int> edges_in  = get_connections(&nodes[n], true);
+      vector<int> edges_out = get_connections(&nodes[n], false);
+
+      vector<int> edges_all = edges_in;
+
+      edges_all.insert(edges_all.end(), edges_out.begin(), edges_out.end());
+      sort(edges_all.begin(), edges_all.end(), greater<>());
+      edges_all.erase(unique(edges_all.begin(), edges_all.end()), edges_all.end());
+
+      if(edges_all.size() == 0) continue;
+
+      for(int & i : edges_in)
+        for(int & j : edges_in)
+          if(i != j)
+            add_edge_or_update_weight(edges.at(i).in, edges.at(j).in, edges.at(i).weight + edges.at(j).weight);
+
+      for(int & i : edges_out)
+        for(int & j : edges_out)
+          if(i != j)
+            add_edge_or_update_weight(edges.at(i).out, edges.at(j).out, edges.at(i).weight + edges.at(j).weight);
+
+      for(int & i : edges_in)
+        for(int & j : edges_out)
+          if(i != j)
+            add_edge_or_update_weight(edges.at(i).in, edges.at(j).out, edges.at(i).weight + edges.at(j).weight);
+
+      for(int & e : edges_all)
         edges.erase(edges.begin() + e);
-        done = false;
-        break;
-        
-      }
+
+      done = false;
+
+      break;
 
     }
 
+  }
+
+}
+
+template <typename T>
+void Graph<T>::print(vector<vector<char>> & grid){
+
+  for(Edge<T> & edge : edges){
+
+      char tile_in = grid[edge.in->first][edge.in->second];
+      char tile_out = grid[edge.out->first][edge.out->second];
+
+      cout<<tile_in<<" ("<<edge.in->first<<", "<<edge.in->second<<") -> "<<tile_out<<" ("<<edge.out->first<<", "<<edge.out->second<<") "<<edge.weight<<endl;
+    
   }
 
 }
@@ -177,6 +256,11 @@ int main(){
 
   make_graph(grid, graph);
 
+  graph.print(grid);
+
   graph.prune(grid);
+
+  cout<<endl;
+  graph.print(grid);
 
 }
