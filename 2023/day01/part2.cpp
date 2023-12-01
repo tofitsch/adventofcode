f8c92543dcdce14950f1a2cdc1bde5d5b11190de
tofitsch <tobias.fitschen@cern.ch>
2023-12-01 15:14:22 +0000

    2023day01

--- 2023/day01/part2.cpp
+++ 2023/day01/part2.cpp
@@ -5,6 +5,7 @@
 using namespace std;
 
 vector<string> digits = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+vector<string> rdigits = {"orez", "eno", "owt", "eerht", "ruof", "evif", "xis", "neves", "thgie", "enin"};
 
 int main(){
 
@@ -17,45 +18,57 @@ int main(){
 
   regex rex(regex_str);
 
+  string rregex_str(regex_str.rbegin(), regex_str.rend());
+  regex rrex(rregex_str);
+
   map<string, string> regex_map;
-  for(int i = 0; i < digits.size(); ++i)
+  for(int i = 0; i < digits.size(); ++i){
     regex_map.insert(make_pair(digits[i], to_string(i)));
+    regex_map.insert(make_pair(rdigits[i], to_string(i)));
+  }
 
   int sum = 0;
   
   string line;
 
-  ifstream in_file("example.txt");
+  ifstream in_file("input.txt");
 
   while(getline(in_file, line)){
     
+//    if(line == "fivegfslvcvtvs4")
+//      break;
+    
     smatch match;
-    
-    while(regex_search(line, match, rex))
+
+    string rline(line.rbegin(), line.rend());
+
+    if(regex_search(line, match, rex))
       line = regex_replace(line, rex, regex_map[match.str()], regex_constants::format_first_only);
-    
+
+    if(regex_search(rline, match, rrex))
+      rline = regex_replace(rline, rrex, regex_map[match.str()], regex_constants::format_first_only);
+
     char first_c = ' ';
     char last_c = ' ';
     
-    for(char & c : line){
-      
-      if(isdigit(c)){
-        
-        last_c = c;
-
+    for(char & c : line)
+      if(isdigit(c))
         if(first_c == ' ')
           first_c = c;
 
-      }
-
-    }
+    for(char & c : rline)
+      if(isdigit(c))
+        if(last_c == ' ')
+          last_c = c;
 
     string num(1, first_c);
     num += last_c;
 
+//    cout<<" "<<first_c<<last_c<<endl;
+
     sum += stoi(num);
 
-    cout<<num<<endl;
+//    cout<<num<<endl;
 
   }
 
