8ae902f343329fc03c557f53547486f597c99841
tofitsch <tobias.fitschen@cern.ch>
2023-12-05 08:07:34 +0000

    2023day05part1 draft

---
+++ 2023/day05/part1.cpp
@@ -0,0 +1,95 @@
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+struct Range{
+  
+  int beg, end, offset;
+
+};
+
+struct Rule{
+  
+  vector<Range> ranges;
+
+  void apply(int & val){
+    
+    for(Range & range : ranges)
+      if(range.beg <= val && range.end >= val){
+
+        val += range.offset;
+	break;
+
+      }
+
+  }
+
+};
+
+void read_rule(string & line, vector<Rule> & rules, int & rule_ctr){
+  
+  if(rules.size() < rule_ctr)
+    rules.push_back(Rule());
+  
+  string field;
+
+  stringstream line_stream(line);
+
+  vector<int> nums;
+
+  while(getline(line_stream, field, ' '))
+    nums.push_back(stoi(field));
+
+  rules.back().ranges.push_back(Range{nums[1], nums[1] + nums[2] - 1, nums[0] - nums[1]});
+
+}
+
+void read_seeds(string & line, vector<int> & seeds){
+  
+  string field;
+
+  stringstream line_stream(line);
+
+  while(getline(line_stream, field, ' '))
+    if(isdigit(field[0]))
+      seeds.push_back(stoi(field));
+
+}
+
+int main(){
+  
+  string line;
+
+  ifstream in_file("input.txt");
+  
+  vector<int> seeds;
+  vector<Rule> rules;
+
+  int rule_ctr = 0;
+
+  while(getline(in_file, line)){
+
+    if(line.find("seeds:") != string::npos)
+      read_seeds(line, seeds);
+
+    else if(line.back() == ':')
+      rule_ctr++;
+
+    else if(line.length() > 0)
+      read_rule(line, rules, rule_ctr);
+
+  }
+
+  for(int & seed : seeds)
+    for(Rule & rule : rules)
+      rule.apply(seed);
+
+  sort(seeds.begin(), seeds.end());
+   
+  cout << seeds[0] << endl;
+
+}
