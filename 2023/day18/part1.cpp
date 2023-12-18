e47d181c789f1e804670e68313a30efb78691161
tofitsch <tobias.fitschen@cern.ch>
2023-12-18 13:47:59 +0000

    2023day18part1 using shoelace and picks

--- 2023/day18/part1.cpp
+++ 2023/day18/part1.cpp
@@ -31,21 +31,8 @@ struct Instruction{
 
 struct Coordinate{
 
-  int y;
-  int x;
-
-  int n_y, n_x;
-
-  int min_y = numeric_limits<int>::max();
-  int min_x = numeric_limits<int>::max();
-  int max_y = numeric_limits<int>::min();
-  int max_x = numeric_limits<int>::min();
-
-  int step_ctr;
-
-  Coordinate() : y(0), x(0) {}
-  Coordinate(int y, int x) : y(y), x(x) {}
-  Coordinate(int y, int x, int n_y, int n_x) : y(y), x(x), n_y(n_y), n_x(n_x) {}
+  int y = 0;
+  int x = 0;
 
   void apply(Instruction & instruction){
     
@@ -60,73 +47,27 @@ struct Coordinate{
 
   }
 
-  bool apply_step(Instruction & instruction){
-    
-    switch(instruction.direction){
-
-      case 'U': y--; break;
-      case 'D': y++; break;
-      case 'L': x--; break;
-      case 'R': x++; break;
-
-    };
-
-    step_ctr++;
-
-    return step_ctr < instruction.length;
-
-  }
-
-  void update_limits(Instruction & instruction){
-
-    apply(instruction);
-
-    if(y > max_y) max_y = y;
-    if(x > max_x) max_x = x;
-    if(y < min_y) min_y = y;
-    if(x < min_x) min_x = x;
-
-  }
-
-  void normalise(){
-
-    y = -min_y + 1 ;
-    x = -min_x + 1;
-
-    n_y = max_y - min_y + 1;
-    n_x = max_x - min_x + 1;
-
-  }
-
-  void update_grid(Instruction & instruction, vector<string> & grid){
-    
-    step_ctr = 0;
-    
-    do grid[y][x] = '#';
-    while(apply_step(instruction));
-
-  }
-
 };
 
-void flood_fill(vector<string> & grid, int & sum, Coordinate coord){
+int shoe_lace_formula(vector<Coordinate> vertices){
   
-  if(grid[coord.y][coord.x] != '.')
-    return;
+  int sum = 0;
 
-  grid[coord.y][coord.x] = 'x';
+  vertices.push_back(vertices[0]);
 
-  sum++;
+  for(int i=0; i<vertices.size() - 1; i++)
+    sum += vertices.at(i).x * vertices.at(i + 1).y - vertices.at(i + 1).x * vertices.at(i).y;
 
-  if(coord.y > 0)
-    flood_fill(grid, sum, Coordinate(coord.y - 1, coord.x, coord.n_y, coord.n_x));
-  if(coord.x > 0)
-    flood_fill(grid, sum, Coordinate(coord.y, coord.x - 1, coord.n_y, coord.n_x));
-  if(coord.y < coord.n_y + 1)
-    flood_fill(grid, sum, Coordinate(coord.y + 1, coord.x, coord.n_y, coord.n_x));
-  if(coord.x < coord.n_x + 1)
-    flood_fill(grid, sum, Coordinate(coord.y, coord.x + 1, coord.n_y, coord.n_x));
+  vertices.pop_back();
+
+  return sum / 2;
+
+}
+
+int picks_theorem(int area, int circumference){
   
+  return area - circumference / 2 + 1 + circumference;
+
 }
 
 int main(){
@@ -140,25 +81,25 @@ int main(){
   while(getline(in_file, line))
     instructions.push_back(Instruction(line));
 
+  vector<Coordinate> vertices;
+
   Coordinate coord;
 
-  for(Instruction & instruction : instructions)
-    coord.update_limits(instruction);
+  int circumference = 0;
+
+  for(Instruction & instruction : instructions){
+    
+    vertices.push_back(coord);
+
+    circumference += instruction.length;
+
+    coord.apply(instruction);
+
+
+  }
+
+  int area = shoe_lace_formula(vertices);
   
-  coord.normalise();
-
-  vector<string> grid(coord.n_y + 2, string(coord.n_x + 2, '.'));
-
-  for(Instruction & instruction : instructions)
-    coord.update_grid(instruction, grid);
-
-  coord.x = 0;
-  coord.y = 0;
-
-  int sum = 0;
-
-  flood_fill(grid, sum, coord);
-
-  cout << (coord.n_y + 2) * (coord.n_x + 2) - sum << endl;
+  cout << picks_theorem(area, circumference) << endl;
 
 }
