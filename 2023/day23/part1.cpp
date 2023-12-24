9f8b8f1d854856215bfa8bd54581a6b12703b045
tofitsch <tobias.fitschen@cern.ch>
2023-12-24 10:46:09 +0000

    graph pruning

--- 2023/day23/part1.cpp
+++ 2023/day23/part1.cpp
@@ -2,23 +2,12 @@
 #include<iostream>
 #include<fstream>
 #include<vector>
+#include<set>
 #include<limits>
 
 using namespace std;
   
 class Graph{
-
-  public:
-    
-    int n_y, n_x;
-
-    void run_dijkstra(bool);
-    void make_directional();
-    int get_path_length();
-
-    void print();
-
-    Graph(string);
 
   private:
     
@@ -38,6 +27,19 @@ class Graph{
     void link_nodes();
 
     bool static distance_decreasing(const Node *, const Node *);
+
+  public:
+    
+    int n_y, n_x;
+
+    int run_dijkstra();
+    void make_directional();
+    void simplify();
+    void simplify_node(Node *);
+
+    void print();
+
+    Graph(string);
 
 };
 
@@ -61,6 +63,7 @@ struct Graph::Edge{
 struct Graph::Node{
   
   vector<Edge> edges;
+  vector<Edge> simple_edges;
 
   int y, x;
 
@@ -71,8 +74,9 @@ struct Graph::Node{
   bool on_path = false;
   Node * previous = nullptr;
 
+  int n_edges_in = 0;
+
   Node * reset();
-  void link(Node *, int);
 
   Node(int y, int x, char c) : y(y), x(x), c(c) {}
   
@@ -86,12 +90,6 @@ Graph::Node * Graph::Node::reset(){
   previous = nullptr;
 
   return this;
-
-}
-
-void Graph::Node::link(Node * node, int weight){
-  
-  edges.push_back(Edge(node, weight));
 
 }
 
@@ -115,7 +113,7 @@ void Graph::add_nodes(){
   
   start_node = & nodes[0][1];
   end_node = & nodes[n_y - 1][n_x - 2];
-
+  
 }
 
 void Graph::link_nodes(){
@@ -127,16 +125,16 @@ void Graph::link_nodes(){
         continue;
       
       if(y - 1 >= 0 && (nodes[y - 1][x].c == '.' || nodes[y - 1][x].c == '^') && (nodes[y][x].c == '.' || nodes[y][x].c == '^'))
-        nodes[y][x].link(& nodes[y - 1][x], 1);
+        nodes[y][x].edges.push_back(Edge(& nodes[y - 1][x], 1));
       
       if(x - 1 >= 0 && (nodes[y][x - 1].c == '.' || nodes[y][x - 1].c == '<') && (nodes[y][x].c == '.' || nodes[y][x].c == '<'))
-        nodes[y][x].link(& nodes[y][x - 1], 1);
+        nodes[y][x].edges.push_back(Edge(& nodes[y][x - 1], 1));
       
       if(y + 1 < n_y && (nodes[y + 1][x].c == '.' || nodes[y + 1][x].c == 'v') && (nodes[y][x].c == '.' || nodes[y][x].c == 'v'))
-        nodes[y][x].link(& nodes[y + 1][x], 1);
+        nodes[y][x].edges.push_back(Edge(& nodes[y + 1][x], 1));
       
       if(x + 1 < n_x && (nodes[y][x + 1].c == '.' || nodes[y][x + 1].c == '>') && (nodes[y][x].c == '.' || nodes[y][x].c == '>'))
-        nodes[y][x].link(& nodes[y][x + 1], 1);
+        nodes[y][x].edges.push_back(Edge(& nodes[y][x + 1], 1));
 
     }
   }
@@ -149,7 +147,7 @@ bool Graph::distance_decreasing(const Node * a, const Node * b){
 
 }
 
-void Graph::run_dijkstra(bool invert_weights){
+int Graph::run_dijkstra(){
 
   vector<Node *> queue;
 
@@ -174,7 +172,7 @@ void Graph::run_dijkstra(bool invert_weights){
     for(Edge & edge : node->edges){
       if(! edge.node->visited){
         
-	int distance_update = node->distance + edge.weight * (invert_weights ? -1 : 1);
+	int distance_update = node->distance + edge.weight;
 	  
 	if(distance_update < edge.node->distance){
 
@@ -201,6 +199,8 @@ void Graph::run_dijkstra(bool invert_weights){
 
   start_node->on_path = true;
 
+  return end_node->distance;
+
 }
 
 void Graph::make_directional(){
@@ -213,8 +213,10 @@ void Graph::make_directional(){
       vector<Edge> forward_edges;
 
       for(Edge & edge : node->edges)
-        if(edge.node->distance > node->distance)
+        if(edge.node->distance >= node->distance || node->c != '.'){
           forward_edges.push_back(edge);
+          edge.node->n_edges_in++;
+        }
 
       node->edges = forward_edges;
 
@@ -223,23 +225,68 @@ void Graph::make_directional(){
 
 }
 
-int Graph::get_path_length(){
+void Graph::simplify(){
+
+  simplify_node(start_node);
+
+  for(int y=0; y<n_y; y++)
+    for(int x=0; x<n_x; x++)
+      nodes[y][x].edges = nodes[y][x].simple_edges;
+
+}
+
+void Graph::simplify_node(Graph::Node * node){
   
-  return end_node->distance;
+//  cout << node->x << " " << node->y << endl;
+  
+  if(node->simple_edges.size() > 0)
+    return;
+
+  for(Edge & next_edge : node->edges){
+  
+    Node * next = next_edge.node;
+
+    int weight = 1;
+    
+    while(! (next == start_node || next == end_node || next->edges.size() != 1 || next->n_edges_in > 1)){
+
+      next = next->edges.front().node;
+
+      weight++;
+
+    }
+
+    if(next->edges.size() == 0 && next != end_node)
+      continue;
+
+    node->simple_edges.push_back(Edge(next, weight));
+
+    simplify_node(next);
+
+  }
 
 }
 
 void Graph::print(){
+  
+  cout << endl;
 
   string const red = "\033[1;31m";
   string const reset = "\033[0m";
+
+  for(int y=0; y<n_y; y++)
+    for(int x=0; x<n_x; x++)
+     for(Edge & edge : nodes[y][x].edges)
+       cout << y << " " << x << " " << edge.node->y << " " << edge.node->x << endl;
 
   for(int y=0; y<n_y; y++){
 
     for(int x=0; x<n_x; x++){
 
-      if(nodes[y][x].on_path)
-        cout << red << nodes[y][x].c << reset;
+      if(nodes[y][x].edges.size() > 0)
+        cout << red << nodes[y][x].edges.size() << reset;
+      //if(nodes[y][x].on_path)
+      //  cout << red << nodes[y][x].c << reset;
       else
         cout << nodes[y][x].c;
 
@@ -256,13 +303,13 @@ int main(){
   
   Graph graph("example.txt");
 
-  graph.run_dijkstra(false);
+  cout << graph.run_dijkstra() << endl;
 
   graph.make_directional();
 
-  graph.run_dijkstra(true);
+  graph.simplify();
 
-  cout << - graph.get_path_length() << endl;
+//  cout << graph.run_dijkstra() << endl;
 
   graph.print();
 
