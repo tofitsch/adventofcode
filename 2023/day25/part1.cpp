f92f3c7731fb07c2784e05298455af6df247f196
tofitsch <tobias.fitschen@cern.ch>
2024-01-12 15:12:12 +0000

    2023 done

--- 2023/day25/part1.cpp
+++ 2023/day25/part1.cpp
@@ -1,51 +1,42 @@
-#include<algorithm>
 #include<iostream>
 #include<fstream>
 #include<sstream>
 #include<vector>
-#include<map>
+#include<algorithm>
+#include<cstdlib>
 
 using namespace std;
   
 class Graph{
 
   private:
-    
-    struct Edge;
-    struct Node;
 
-    map<string, Node> nodes;
+    struct Edge;
+
+    vector<string> nodes;
     vector<Edge> edges;
 
-    void traverse(Edge *);
-    int count_subgraphs();
-
+    void contract_random_edge();
+   
   public:
     
     Graph(string);
-    int find_subgraphs();
+
+    void run_karger(int);
 
 };
 
 struct Graph::Edge{
 
-  Node * in, * out;
+  string in, out;
 
-  bool active = true;
+  Edge(string in, string out) : in(in), out(out) {}
 
-  Edge(Node * in, Node * out) : in(in), out(out) {}
+  bool operator==(const Edge & other){
+    return (this->in == other.in && this->out == other.out) ||
+           (this->in == other.out && this->out == other.in);
+  }
 
-};
-
-struct Graph::Node{
-  
-  vector<Edge *> edges;
-
-  string label;
-
-  Node() {}
-  Node(string label) : label(label) {}
-  
 };
 
 Graph::Graph(string in_file_name){
@@ -54,105 +45,99 @@ Graph::Graph(string in_file_name){
 
   ifstream in_file(in_file_name);
 
-  vector<vector<string>> lines;
-
   while(getline(in_file, line)){
     
-    lines.push_back({});
-
+    vector<string> fields;
+    
     stringstream line_stream(line);
 
     while(getline(line_stream, field, ' '))
-      lines.back().push_back(field);
+      fields.push_back(field);
 
-    lines.back().front().pop_back();
+    fields[0].pop_back();
 
-    for(string & label : lines.back())
-      if(nodes.find(label) == nodes.end())
-        nodes[label] = Node(label);
+    for(string & n : fields){
 
-  }
+      if(find(nodes.begin(), nodes.end(), n) == nodes.end())
+        nodes.push_back(n);
 
-  for(vector<string> & labels : lines)
-    for(int i=1; i<labels.size(); i++)
-      edges.push_back(Edge(& nodes[labels[0]], & nodes[labels[i]]));
+      if(n == fields[0])
+        continue;
 
-  for(Edge & edge : edges){
-    
-    edge.in->edges.push_back(& edge);
-    edge.out->edges.push_back(& edge);
+      Edge e(fields[0], n);
+      if(find(edges.begin(), edges.end(), e) == edges.end())
+        edges.push_back(e);
 
-  }
-    
-}
-
-void Graph::traverse(Edge * edge){
-  
-  if(! edge->active)
-    return;
-
-  edge->active = false;
-
-  for(Edge * e : edge->in->edges)
-    traverse(e);
-
-  for(Edge * e : edge->out->edges)
-    traverse(e);
-
-}
-
-int Graph::count_subgraphs(){
-  
-  int ctr = 0;
-  
-  for(Edge & edge : edges){
-    
-    if(! edge.active)
-      continue;
-
-    ctr++;
-    
-    traverse(& edge);
-    
-  }
-
-  return ctr;
-
-}
-
-int Graph::find_subgraphs(){
-  
-  for(Edge & a : edges){
-    for(Edge & b : edges){
-      for(Edge & c : edges){
-
-        for(Edge & edge : edges)
-          edge.active = true;
-
-        a.active = false;
-        b.active = false;
-        c.active = false;
-
-        if(count_subgraphs() == 2){
-          cout << a.in->label << " " << a.out->label << " ";
-          cout << b.in->label << " " << b.out->label << " ";
-          cout << c.in->label << " " << c.out->label << " ";
-          cout << endl;
-          exit(0); //TODO
-        }
-
-      }
     }
+
+  }
+  
+}
+
+void Graph::contract_random_edge(){
+  
+  int id = rand() % edges.size();
+
+  Edge edge = edges[id];
+ 
+  edges.erase(remove(edges.begin(), edges.end(), edge), edges.end());
+
+  nodes.erase(remove(nodes.begin(), nodes.end(), edge.in), nodes.end());
+  nodes.erase(remove(nodes.begin(), nodes.end(), edge.out), nodes.end());
+
+  string merged_node = edge.in + ',' + edge.out;
+
+  nodes.push_back(merged_node);
+
+  for(Edge & e : edges){
+    
+    if(e.in == edge.in || e.in == edge.out)
+      e.in = merged_node;
+
+    if(e.out == edge.in || e.out == edge.out)
+      e.out = merged_node;
+
   }
 
-  return 0;
+}
+
+void Graph::run_karger(int n_cuts){
+  
+  vector<string> nodes_original = nodes;
+  vector<Edge> edges_original = edges;
+
+  while(edges.size() != n_cuts){
+
+    nodes = nodes_original;
+    edges = edges_original;
+
+    while(nodes.size() > 2)
+      contract_random_edge();
+
+  }
+
+  int result = 1;
+
+  for(int i=0; i<2; i++){
+
+    int n_nodes = 1;
+
+    for(char c : nodes[i])
+      if(c == ',')
+        n_nodes++;
+
+    result *= n_nodes;
+
+  }
+
+  cout << result << endl;
 
 }
 
 int main(){
-  
-  Graph graph("example.txt");
+    
+  Graph graph("input.txt");
 
-  cout << graph.find_subgraphs() << endl;
+  graph.run_karger(3);
 
 }
