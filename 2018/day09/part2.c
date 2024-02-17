7ab9132961fb2d21d0b82230d71c214cf172d217
tofitsch <tobias.fitschen@cern.ch>
2024-02-17 18:13:12 +0100

    2018day09part2

---
+++ 2018/day09/part2.c
@@ -0,0 +1,113 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LINE_LENGTH 64
+
+typedef struct dll_node dll_node;
+
+struct dll_node{
+
+  long value;
+
+  dll_node * next;
+  dll_node * prev;
+
+};
+
+void read_line(char * line, int * n_players, int * n_marbels){
+  
+  char * ptr = line;
+
+  while(* ptr != ' ')
+    ptr++;
+
+  * ptr = '\0';
+
+  * n_players = atoi(line);
+
+  ptr += sizeof("players; last marble is worth ");
+
+  line = ptr;
+
+  while(* ptr != ' ')
+    ptr++;
+
+  * ptr = '\0';
+
+  * n_marbels = atoi(line) + 1;
+
+}
+
+int main(){
+
+  FILE * in_file = fopen("input.txt", "r");
+
+  char line[MAX_LINE_LENGTH];
+
+  fgets(line, sizeof(line), in_file);
+
+  int n_players, n_marbels;
+
+  read_line(line, & n_players, & n_marbels);
+
+  n_marbels *= 100;
+
+  fclose(in_file);
+
+  long score[n_players];
+
+  memset(score, 0, sizeof(score));
+
+  dll_node node;
+
+  node.value = 0;
+
+  node.next = & node;
+  node.prev = & node;
+
+  dll_node * current = & node;
+
+  for(long i=1; i<n_marbels; i++){
+    
+    if(i % 23 == 0){
+      
+      int player = (i - 1) % n_players;
+
+      for(int j=0; j<6; j++)
+        current = current->prev;
+
+      score[player] += i + current->prev->value;
+
+      current->prev->prev->next = current;
+
+      current->prev = current->prev->prev;
+
+    }
+    else{
+
+      dll_node * new_node = (dll_node *) malloc(sizeof(dll_node));
+
+      new_node->value = i;
+
+      new_node->prev = current->next;
+      new_node->next = current->next->next;
+
+      current = new_node;
+
+      current->prev->next = new_node;
+      current->next->prev = new_node;
+
+    }
+
+  }
+
+  long max = 0;
+
+  for(int i=0; i<n_players; i++)
+    if(score[i] > max)
+      max = score[i];
+
+  printf("%li\n", max);
+
+}
