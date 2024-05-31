d352929ac90487cc179c9dba314dbc2748a8a91d
tofitsch <tobias.fitschen@cern.ch>
2024-05-31 15:00:43 +0100

    2018day22part1 draft works for example

--- 2018/day22/part1.c
+++ 2018/day22/part1.c
@@ -3,6 +3,9 @@
 #include <string.h>
 
 #define MAX_LINE_LENGTH 64
+#define MOD 20183
+#define FAC_X 48271
+#define FAC_Y 16807
 
 void read_input(int * depth, int * target_x, int * target_y) {
   
@@ -50,51 +53,21 @@ int main() {
 
   read_input(& depth, & target_x, & target_y);
 
-//  printf("%i, %i, %i\n", depth, target_x, target_y);
+  printf("%i, %i, %i\n", depth, target_x, target_y);
 
   int  erosion_lvl[target_y + 1][target_x + 1];
   char   tile_type[target_y + 1][target_x + 1];
 
-  int geo_idx;
-
   for(int x=0; x<=target_x; ++x)
-    erosion_lvl[0][x] = (x * 16807 + depth) % 20183;
+    erosion_lvl[0][x] = (x * FAC_Y + depth) % MOD;
 
   for(int y=0; y<=target_y; ++y)
-    erosion_lvl[y][0] = (y * 48271 + depth) % 20183;
-
-  //anti-diagonal traversal
-
-  //anti-diagonal traversal
-//  int const Y = 4;
-//  int const X = 2;
-//  int arr[4][2] = {1, 2,
-//                   3, 4,
-//                   5, 6,
-//                   7, 8};
-  
-  int const Y = 4;
-  int const X = 3;
-  int arr[4][3] = {{ 1,  2,  3},
-                   { 4,  5,  6},
-                   { 7,  8,  9},
-                   {10, 11, 12}};
-
-  for(int y=0; y<Y; ++y) {
-
-    for(int x=0; x<X; ++x)
-      printf("%i", arr[y][x]);
-    
-    printf("\n");
-
-  }
-
-  printf("\n");
+    erosion_lvl[y][0] = (y * FAC_X + depth) % MOD;
 
   /*
      anti-diagonal traversal
 
-     consider e.g. the matrix
+     consider e.g. the matrix arr[Y=4][X=3]:
 
      123
      456
@@ -108,18 +81,68 @@ int main() {
      xx789x
      xxxABC
 
-     and traversing top to bottom for each column
+     and traversing top to bottom for each column traverses anti-diagonally over the whole matrix:
+
+     for(int x = 0; x < X + Y - 1; ++x)
+       for(int y = (x >= X ? x - X + 1 : 0); y < (x <= X - 1 ? x + 1 : Y); ++y)
+         printf("%i ", arr[y][x - y]);
+
+     1
+     2 4
+     3 5 7
+     6 8 A
+     9 B
+     C
+
+
+     in addition to that, the following skips row 0 and column 0 of the original matrix
 
   */
 
 
-  for(int x = 0; x < X + Y - 1; ++x) {
-    
-    for(int y = (x >= X ? x - X + 1 : 0); y < (x <= X - 1 ? x + 1 : Y); ++y)
-      printf("%i ", arr[y][x - y]);
-    
+  int X = target_x + 1;
+  int Y = target_y + 1;
+
+  for(int x = 0; x < X + Y - 1; ++x)
+    for(int y = (x >= X ? x - X + 1 : 1); y < (x <= X ? x : Y); ++y)
+      erosion_lvl[y][x - y] = (erosion_lvl[y - 1][x - y] * erosion_lvl[y][x - y - 1] + depth) % MOD;
+
+  erosion_lvl[target_y][target_x] = depth % MOD;
+
+/*
+  for(int y=0; y<=target_y; ++y) {
+
+    for(int x=0; x<=target_x; ++x) {
+
+      switch(erosion_lvl[y][x] % 3) {
+        
+        case 0:
+          printf(".");
+          break;
+
+        case 1:
+          printf("=");
+          break;
+
+        case 2:
+          printf("|");
+          break;
+
+      };
+
+    }
+
     printf("\n");
 
   }
+ */
+  
+  int risk_lvl = 0;
+
+  for(int y=0; y<=target_y; ++y)
+    for(int x=0; x<=target_x; ++x)
+      risk_lvl += erosion_lvl[y][x] % 3;
+
+  printf("%i\n", risk_lvl);
 
 }
